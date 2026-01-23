a3d821daa7c76c2d753ba51ec44019af956561d2
Jayden <xupeiyu0827@gmail.com>
2026-01-23 12:27:17 -0500

    remove redudant imports

--- flash_mla/kernels/decode_core.py
+++ flash_mla/kernels/decode_core.py
@@ -1,6 +1,5 @@
 # FlashDecoding + MLA
 
-import torch
 import triton
 import triton.language as tl
 
--- flash_mla/kernels/prefill_core.py
+++ flash_mla/kernels/prefill_core.py
@@ -1,6 +1,5 @@
 # FlashAttention + MLA
 
-import torch
 import triton
 import triton.language as tl
 
